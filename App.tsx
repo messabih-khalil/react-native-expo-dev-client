babf192a68878b1688a3cfc6b3586b71990b75c8
messabih <msbih.khalil@gmail.com>
2023-12-05 22:09:13 +0100

    init : try to link native libs dont provided by expo sdk

--- App.tsx
+++ App.tsx
@@ -1,20 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
+import { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NetworkInfo } from 'react-native-network-info';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    const [myIp, setMyIp] = useState<string>('');
+
+    useEffect(() => {
+        NetworkInfo.getIPAddress().then((ipAddress: any) => {
+            console.log(ipAddress);
+            setMyIp(ipAddress);
+        });
+    }, []);
+
+    return (
+        <View style={styles.container}>
+            <Text>My Ip Is : {myIp}</Text>
+            <StatusBar style='auto' />
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
