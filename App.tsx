05c792d0ac4af0fb81947bc8362be432110c3475
messabih <msbih.khalil@gmail.com>
2023-12-05 23:27:44 +0100

    init : try to link native libs dont provided by expo sdk

--- App.tsx
+++ App.tsx
@@ -1,31 +1,121 @@
-import { StatusBar } from 'expo-status-bar';
-import { useEffect, useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, Button, TextInput } from 'react-native';
+import UdpSocket from 'react-native-udp';
 import { NetworkInfo } from 'react-native-network-info';
-
 export default function App() {
-    const [myIp, setMyIp] = useState<string>('');
-
+    const [isServer, setIsServer] = useState(false);
+    const [connectionStatus, setConnectionStatus] = useState('');
+    const [socket, setSocket] = useState('');
+    const [ipAddress, setIpAddress] = useState<string | null>('');
+    const [mensaje, setMensaje] = useState('');
+    const [ipServer, setIpServer] = React.useState('IP Server');
     useEffect(() => {
-        NetworkInfo.getIPAddress().then((ipAddress: any) => {
-            console.log(ipAddress);
-            setMyIp(ipAddress);
+        const fetchIpAddress = async () => {
+            const ip: string | null = await NetworkInfo.getIPV4Address();
+            setIpAddress(ip);
+        };
+
+        fetchIpAddress();
+
+        if (isServer) {
+            // Configura la aplicación como servidor
+            // @ts-ignore
+            const server = UdpSocket.createSocket('udp4');
+
+            server.on('message', (data, rinfo) => {
+                setMensaje(data.toString());
+                server.send(
+                    '¡Hola desde el servidor!',
+                    undefined,
+                    undefined,
+                    rinfo?.port,
+                    rinfo?.address,
+                    (error) => {
+                        if (error) {
+                            console.log('Error al enviar el mensaje:', error);
+                        } else {
+                            console.log('Mensaje enviado correctamente');
+                        }
+                    }
+                );
+                console.log('Mensaje recibido:', data.toString());
+            });
+
+            server.on('listening', () => {
+                console.log(
+                    'Servidor escuchando en el puerto:',
+                    server.address().port
+                );
+                setConnectionStatus(
+                    `Servidor escuchando en el puerto ${server.address().port}`
+                );
+            });
+
+            server.bind(8888);
+            // @ts-ignore
+            setSocket(server);
+        } else {
+            setConnectionStatus(`Servidor desconectado`);
+            // Configura la aplicación como cliente
+            // @ts-ignore
+            const client = UdpSocket.createSocket('udp4');
+            client.bind(8887);
+            // @ts-ignore
+            setSocket(client);
+        }
+
+        return () => {
+            // @ts-ignore
+            socket && socket.close();
+        };
+    }, [isServer]);
+
+    const sendMessage = () => {
+        if (isServer) return;
+
+        const client = socket;
+        // @ts-ignore
+        client.send(
+            '¡Hola desde el cliente!',
+            undefined,
+            undefined,
+            8888,
+            ipServer,
+            // @ts-ignore
+            (error) => {
+                if (error) {
+                    console.log('Error al enviar el mensaje:', error);
+                } else {
+                    console.log('Mensaje enviado correctamente');
+                }
+            }
+        );
+        // @ts-ignore
+        client.on('message', async (message, remoteInfo) => {
+            setMensaje(message.toString());
         });
-    }, []);
+    };
 
     return (
-        <View style={styles.container}>
-            <Text>My Ip Is : {myIp}</Text>
-            <StatusBar style='auto' />
+        <View
+            style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}
+        >
+            <Text>{connectionStatus}</Text>
+            <Button
+                title={isServer ? 'Detener Servidor' : 'Iniciar Servidor'}
+                onPress={() => setIsServer(!isServer)}
+            />
+            <Button
+                title='Enviar Mensaje'
+                onPress={sendMessage}
+                disabled={isServer}
+            />
+            <TextInput
+                onChangeText={setIpServer}
+                value={ipServer}
+            />
+            <Text>Dirección IP: {ipAddress}</Text>
+            <Text>Mensaje recibído: {mensaje}</Text>
         </View>
     );
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-});
